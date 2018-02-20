2ea52b9bf8dead91969d144edaa37b8ea0add282
Boloutare Doubeni (Tare) <tarecd@gmail.com>
2018-02-20 00:02:23 -0500

    Add nil and bool parser

--- src/EDN/Library.fs
+++ src/EDN/Library.fs
@@ -23,6 +23,48 @@ module Types =
         | Tag of tag: Symbol * repr: EDN
         | Discard of element: EDN
 
+module Token =
+    let [<Literal>] Nil = "nil"
+    let [<Literal>] True = "true"
+    let [<Literal>] False = "false"
+
+open FParsec
+
+module private Lexer =
+    open Token
+
+    type UserState = unit
+
+    let parseNil = skipString Nil
+    let parseTrue = skipString True >>% true
+    let parseFalse = skipString False >>% false
+
+
+module private Parser =
+    open Lexer
+    open Types
+
+    type UserState = unit
+
+    let parser : Parser<EDN, UserState> =
+        let pNil = parseNil |>> fun () -> Nil
+        let parseBool = parseFalse <|> parseTrue |>> Boolean
+        choice [
+            pNil
+            parseBool
+        ]
+
+[<AutoOpen>]
+module FParsecUtil =
+    let parseResultToFSResult =
+        function
+        | ParserResult.Success (result, userstate, position) -> Result.Ok (result, userstate, position)
+        | Failure (message, error, userstate) -> Result.Error (message, error, userstate)
+
+[<RequireQualifiedAccess>]
+module Driver =
+    open Parser
+    let runParser input = run parser input |> parseResultToFSResult
 
 module Say =
     let hello name =
--- tests/EDN.Tests/Tests.fs
+++ tests/EDN.Tests/Tests.fs
@@ -4,6 +4,45 @@ module Tests
 open Expecto
 open EDN
 
+module ParserTests =
+
+    let private (|ParsedResultOk|_|) =
+        function
+        | Result.Ok (t, _, _) -> Some t
+        | _ -> None
+
+    let private testNil =
+        test "nil" {
+            let input = Token.Nil
+            let (ParsedResultOk result) = Driver.runParser input
+            let expected = EDN.Types.Nil
+            Expect.equal expected result "nil can be parsed"
+        }
+
+     let private testTrue =
+        test "true" {
+            let input = Token.True
+            let (ParsedResultOk result) = Driver.runParser input
+            let expected = EDN.Types.Boolean true
+            Expect.equal expected result "true can be parsed"
+        }
+
+    let private testFalse =
+        test "false" {
+            let input = Token.False
+            let (ParsedResultOk result) = Driver.runParser input
+            let expected = EDN.Types.Boolean false
+            Expect.equal expected result "false can be parsed"
+        }
+
+    [<Tests>]
+    let parseTests =
+        testList "parser tests" [
+            testNil
+            testTrue
+            testFalse
+        ]
+
 [<Tests>]
 let tests =
   testList "samples" [
