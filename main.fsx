e8b967c2405db8992a4e5b9494feeea9eadb4064
Boloutare Doubeni (Tare) <tarecd@gmail.com>
2018-02-23 17:28:41 -0500

    Simplify script

--- main.fsx
+++ main.fsx
@@ -1,3 +1,6 @@
 #!/usr/bin/env fsharpi
 // References generated via `mono .paket/paket.exe generate-load-scripts`
-#load ".paket/load/netstandard2.0/main.group.fsx"
+#r "src/EDN/bin/Release/netstandard2.0/EDN.dll"
+open EDN.Types
+
+do EDN.Nil |> ignore
